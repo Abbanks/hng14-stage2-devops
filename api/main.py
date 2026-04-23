16f7852a700d5f6c5c8ba89bddcff40663170eb3
Oluwabanke Eboda <abeegarl@gmail.com>
2026-04-23 03:29:50 +0100

    create a gitignore file, add .env file and use environment variables in main.py

--- api/main.py
+++ api/main.py
@@ -5,7 +5,11 @@ import os
 
 app = FastAPI()
 
-r = redis.Redis(host="localhost", port=6379)
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 
 @app.post("/jobs")
 def create_job():
