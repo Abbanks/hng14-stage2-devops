5f84f1fca057b563daa9b54c783ae0c7790e4e91
Oluwabanke Eboda <abeegarl@gmail.com>
2026-04-23 21:38:06 +0100

    ci cd issues

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import redis
 import uuid
 import os
@@ -22,5 +22,5 @@ def create_job():
 def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
     if not status:
-        return {"error": "not found"}
+        raise HTTPException(status_code=404, detail="Job not found")
     return {"job_id": job_id, "status": status.decode('utf-8')}
